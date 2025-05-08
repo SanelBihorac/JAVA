a180c0d78e947233f6ae6efffe3af877029c11a5
SanelBihorac <sbihorac85@gmail.com>
2025-05-08 16:43:16 +0200

    firs commit

---
+++ 1_Getting_Started/Challenge 1/YourInitials.java
@@ -0,0 +1,14 @@
+package Challenge;
+//Print your initials (see your_initials.png and guide.png)
+
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println(" SSS   BBBB ");
+        System.out.println("S   S  B   B");
+        System.out.println("S      B   B");
+        System.out.println(" SSS   BBBB ");
+        System.out.println("    S  B   B");
+        System.out.println("S   S  B   B");
+        System.out.println(" SSS   BBBB ");
+    }
+}
